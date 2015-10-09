f542264deff76fb446be2bf1a2ba51c2889fdb16
UCSC Movil <ucscmovil@MacBook-Pro-de-UCSC.local>
2015-10-08 22:33:08 -0300

    Initial Commit

---
+++ Spotify/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Spotify
+//
+//  Created by UCSC Movil on 08-10-15.
+//  Copyright (c) 2015 XimenaArriagada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Spotify/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Spotify
+//
+//  Created by UCSC Movil on 08-10-15.
+//  Copyright (c) 2015 XimenaArriagada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
